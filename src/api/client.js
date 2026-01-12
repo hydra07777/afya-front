56096005524e71488234bdd05d2ef8568b9bc692
hydra <16paysayocesar@gmail.com>
2026-01-12 19:25:08 +0100

    vercel fix spa II

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-
-
 const api = axios.create({
-    baseURL: `${import.meta.env.PROD}/api`,
+    baseURL: import.meta.env.PROD
+        ? '/api'
+        : 'http://localhost:4000/api',
 });
 
 export default api;
